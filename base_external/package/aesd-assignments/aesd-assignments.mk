6527c03e95498ee577a5c53f9b6aa242f036285c
sana-all <magigingbestlawyerako21@gmail.com>
2025-10-28 00:11:15 +0800

    commit 11

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -11,7 +11,8 @@ AESD_ASSIGNMENTS_VERSION = '92cf3a9c449161e4e09d096ffa800a8a9187c252'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-sana-all.git
+# AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-sana-all.git
+AESD_ASSIGNMENTS_SITE = https://github.com/cu-ecen-aeld/assignments-3-and-later-sana-all.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
