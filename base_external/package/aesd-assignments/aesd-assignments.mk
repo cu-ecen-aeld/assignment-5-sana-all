5522ae852d470a044bc2f3f879be70574fa3254a
sana-all <magigingbestlawyerako21@gmail.com>
2025-10-29 14:17:05 +0800

    commit 28

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -8,7 +8,8 @@
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 # AESD_ASSIGNMENTS_VERSION = '1cc47bfa4eb94f71ca9a15ca06ec1b9fedc4cca4'
 # AESD_ASSIGNMENTS_VERSION = '92cf3a9c449161e4e09d096ffa800a8a9187c252'
-AESD_ASSIGNMENTS_VERSION = '4d2aeacd68afa8a2d09b3bdc1e918ed666fa2a2a'
+# AESD_ASSIGNMENTS_VERSION = '4d2aeacd68afa8a2d09b3bdc1e918ed666fa2a2a'
+AESD_ASSIGNMENTS_VERSION = '4598a75c864ce6908b260d68d1d1f3b555c8035e'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -18,18 +19,18 @@ AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-  	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+#  	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-  	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
-  	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-  	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
-  	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin
-  	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin
-  	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin
+#  	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
+#  	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
+#  	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+#  	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin
+#  	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin
+#  	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin
 	echo i will now start to human debug
 	echo $(@D) 
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
